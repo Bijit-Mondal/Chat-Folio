1571db93288b7cc268d37f4eb9155f8ea1e7f3a5
Bijit Mondal <bijitmondal3011@rediffmail.com>
2023-09-24 09:03:07 +0530

    It Works

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,30 @@
-import { Hono } from 'hono'
+import { Hono, Context } from 'hono';
+import { run } from './openai';
+import { serveStatic } from 'hono/bun'
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => c.text('Hello Hono!'))
+app.use('/views/', serveStatic({ root:  'views' }));
 
-export default app
+
+app.get('/', serveStatic({ path: 'views/chat.html' }))
+
+app.post('/answer', async (c: Context) => {
+  try {
+    let obj: { prompt?: string } = await c.req.json();
+    let prompt: string | undefined = obj.prompt;
+
+    if (!prompt) {
+      throw new Error('Prompt is missing');
+    }
+
+    console.log('Prompt:', prompt);
+    const res = await run([prompt]);
+    return c.json(res);
+  } catch (error) {
+    console.error('Error:', error);
+    return c.status(418);
+  }
+});
+
+export default app;
